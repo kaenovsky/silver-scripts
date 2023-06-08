d48ef83ff96f107db3e4251d0f9cc3e92a806a33
kaenovsky <martinkaen@gmail.com>
2023-06-08 12:04:10 -0300

    practice js functions

--- functions.js
+++ functions.js
@@ -116,5 +116,118 @@ const myMath = {
     },
     cube: function(num) {
         return num ** 3;
+    },
+    // shorthand
+    add(x, y) {
+        return x + y;
     }
 }
+
+console.log(`using my custom add method to add 5+2: ${myMath.add(5,2)}`);
+
+// Coding quiz method
+
+const square = {
+    area(sideLen) {
+        return sideLen * sideLen;
+    },
+    perimeter(side) {
+        return side * 4;
+    }
+}
+
+// this keyword
+
+const cat = {
+    name: 'blanquito',
+    color: 'white',
+    meow() {
+        console.log('~~~ meow meow ~~~');
+    },
+    sayColor() {
+        console.log(`my color is ${this.color}`);
+    }
+}
+
+// egg laying code exercise
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount++;
+        return 'EGG';
+    }
+}
+
+// Try catch error handling
+
+function yell(word) {
+    try {
+        wordToYell = word.toUpperCase().repeat(3);
+        console.log(wordToYell);
+    } catch (error) {
+        console.log('error found! Are you using a string? See error below: ');       
+        console.log('**************')
+        console.log(error);
+        console.log('**************')
+    }
+}
+
+// forEach
+
+const numbs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbs.forEach(function(el) {
+    console.log(el);
+})
+
+// use map to double every number of array
+
+const doubles = numbs.map(function(num) {
+    return num * 2;
+})
+
+console.log(doubles);
+
+// example with movies
+
+const movies = [
+    {
+        title: 'The Matrix',
+        score: 96
+    },
+    {
+        title: 'V for Vendetta',
+        score: 79
+    },
+    {
+        title: 'City of God',
+        score: 88
+    }
+]
+
+const titles = movies.map(function(movie) {
+    return movie.title;
+})
+
+console.log(titles);
+
+// Arrow functions
+
+const addNumbers = (x, y) => {
+    return x + y;
+}
+
+const squareFunc = (num) => {
+    return num ** 2;
+}
+
+const rollOtherDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+// implicit return syntax example in one line
+
+const isEven = (x) => (x % 2 === 0);
+const multiply = (x,y) => (x * y);
