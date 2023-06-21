b2ef8bea8f019293a80686ee541c94d8ef08d16e
kaenovsky <martinkaen@gmail.com>
2023-06-21 15:52:53 -0300

    pokemon task from scratch

---
+++ demo-dom/PokemonNew/app.js
@@ -0,0 +1,25 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+let baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i <= 151; i++) {
+
+    // create HTML elements
+    const div = document.createElement('div');
+    const subText = document.createElement('span');
+    subText.innerText = i;
+    
+    // create image
+    const newImg = document.createElement('img');
+    newImg.src = `${baseUrl}${i}.png`;
+
+    // add elements to markup
+    container.appendChild(div);
+    div.appendChild(newImg);
+    div.appendChild(subText);
+
+    // add style to div element
+    div.classList.add('pokemon');
+    
+}
