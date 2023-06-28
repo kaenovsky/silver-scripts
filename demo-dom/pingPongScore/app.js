b8be5cf1dfee1e7180baddd351bc1d51b6636b78
kaenovsky <martinkaen@gmail.com>
2023-06-28 16:00:33 -0300

    js++

---
+++ demo-dom/pingPongScore/app.js
@@ -0,0 +1,54 @@
+// select score elements
+const score1 = document.querySelector('#score1');
+const score2 = document.querySelector('#score2');
+let pointsP1 = 0;
+let pointsP2 = 0;
+
+// select buttons
+const btn1 = document.querySelector('#player1');
+const btn2 = document.querySelector('#player2');
+const reset = document.querySelector('#reset');
+const playingTo = document.querySelector('#playingto');
+
+// score logic
+
+let maxScore = 5;
+
+playingTo.addEventListener('change', (e) => {
+    maxScore = e.target.value;
+    console.log('max score is now ----> ' + maxScore);
+})
+
+btn1.addEventListener('click', () => {
+    if(pointsP1 < maxScore && pointsP2 < maxScore) {
+        pointsP1 = pointsP1 + 1;
+        score1.innerHTML = pointsP1;
+    }
+
+    if (pointsP1 == maxScore) {
+        score1.style.color = 'green';
+        score2.style.color = 'red';
+    }
+})
+
+btn2.addEventListener('click', () => {
+    if(pointsP1 < maxScore && pointsP2 < maxScore) {
+        pointsP2 = pointsP2 + 1;
+        score2.innerHTML = pointsP2;
+    }
+
+    if (pointsP2 == maxScore) {
+        score2.style.color = 'red';
+        score1.style.color = 'green';
+    }
+})
+
+reset.addEventListener('click', () => {
+    pointsP1 = 0;
+    pointsP2 = 0;
+    score1.innerHTML = pointsP1;
+    score2.innerHTML = pointsP2;
+    score1.style.color = 'unset';
+    score2.style.color = 'unset';
+})
+
