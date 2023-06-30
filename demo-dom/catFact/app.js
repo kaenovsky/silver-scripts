51f414ced3b602dc50ab1d4ac4d1322cb9f9cc21
kaenovsky <martinkaen@gmail.com>
2023-06-30 17:08:29 -0300

    refactor async

--- demo-dom/catFact/app.js
+++ demo-dom/catFact/app.js
@@ -1,14 +1,29 @@
 const fact = document.querySelector('#fact');
 
-fetch('https://catfact.ninja/fact')
-.then((res) => {
-    console.log('response ok!', res);
-    return res.json();
-})
-.then((data) => {
-    console.log(data);
-    fact.innerHTML = data.fact;
-})
-.catch((err) => {
-    console.log('There is an error: ', err);
-})
+// fetch('https://catfact.ninja/fact')
+// .then((res) => {
+//     console.log('response ok!', res);
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data);
+//     fact.innerHTML = data.fact;
+// })
+// .catch((err) => {
+//     console.log('There is an error: ', err);
+// })
+
+// refactor with async()
+
+const getCatFact = async () => {
+    try {
+        const res = await fetch('https://catfact.ninja/fact');
+        const data = await res.json();
+        console.log(data);
+        fact.innerHTML = data.fact;
+    } catch (error) {
+        console.log('Error! --> ', error);        
+    }
+}
+
+getCatFact();
