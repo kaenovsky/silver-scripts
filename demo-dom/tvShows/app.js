e7302c480ceb3bb6f632eca9e2ea2da852333bb5
kaenovsky <martinkaen@gmail.com>
2023-07-03 12:44:10 -0300

    js++

---
+++ demo-dom/tvShows/app.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('#searchForm');
+const input = document.querySelector('input');
+const container = document.querySelector('#container');
+
+// get user input search
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // clear container
+    container.innerHTML = '';
+    // search
+    const searchTerm = form.elements.query.value;
+    getShows(searchTerm);           
+})
+
+// get tv shows with axios
+
+const getShows = async (search) => {
+    const url = `https://api.tvmaze.com/search/shows?q=${search}`;
+    try {
+        const res = await axios.get(url);
+        const shows = res.data;
+        for (s of shows) {
+            console.log(s.show.image.medium);
+            const img = document.createElement('IMG');
+            img.src = s.show.image.medium;
+            container.append(img);
+    }        
+    } catch (error) {
+        console.log('There was an error: ', error);
+        const errMsg = document.createElement('p');
+        errMsg.classList.add('errMsg');
+        errMsg.innerText = `There was an error. Some shows may not display: \ ${error}`;
+        container.append(errMsg);
+    }    
+}
