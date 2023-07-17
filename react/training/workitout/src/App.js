f997871edcde10b6b06a7ac35c155abd093b3869
kaenovsky <martinkaen@gmail.com>
2023-07-17 16:25:47 -0300

    js +

--- react/training/workitout/src/App.js
+++ react/training/workitout/src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <SlotMachine val1="🍒" val2="🍒" val3="💀" />
+      <SlotMachine val1="🍒" val2="🥑" val3="🍒" />
+      <SlotMachine val1="💀" val2="💀" val3="🍒" />
+      <SlotMachine val1="🥑" val2="🥑" val3="🥑" />
+      <SlotMachine val1="🍒" val2="🍌" val3="🍌" />
       <SlotMachine val1="🍒" val2="🍒" val3="🍒" />
       {/* <ColorList colors={["red", "pink", "indigo"]} />
       <ColorList colors={["darkblue", "gold", "teal"]} /> */}
--- react/training/workitout/src/Slot.jsx
+++ react/training/workitout/src/Slot.jsx
@@ -2,12 +2,12 @@ import "./Slot.css";
 
 export default function SlotMachine({ val1, val2, val3 }) {
     const isWinner = val1 === val2 && val2 === val3;
-    const fontColor = isWinner ? "green" : "red";
+    const fontColor = isWinner ? "green" : "crimson";
     return (
         <div className="slotBox">
-            <h1>==| {val1} | {val2} | {val3} |==</h1>
-            <h2>{isWinner ? "You win!" : "You lose!"}</h2>
-            <h3 style={{color: fontColor}}>{isWinner ? "Congrats" : "Better luck next time"}</h3>
+            <h1>{val1}  {val2}  {val3}</h1>
+            <h2 style={{color: fontColor}}>{isWinner ? "You win!" : "You lose!"}</h2>
+            {isWinner && <h3>Congratulations!</h3>}
         </div>
     );
 }
