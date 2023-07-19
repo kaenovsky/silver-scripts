55e3a2f53e7857d20ab686d6ec4f4fa131687476
kaenovsky <martinkaen@gmail.com>
2023-07-19 09:49:12 -0300

    Add title to h1

--- react/training/airbnbish/src/Property.jsx
+++ react/training/airbnbish/src/Property.jsx
@@ -3,7 +3,7 @@ import "./Property.css";
 function Property({ name, rating, price }) {
     return (
         <div className="property">
-            <h1>{name}</h1>
+            <h1 title={name}>{name}</h1>
             <h2>${price} a night</h2>
             <h3>{rating} ⭐️</h3>
         </div>
