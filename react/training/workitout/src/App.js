0986e70226b36719526336972e9bd6bade6fa736
kaenovsky <martinkaen@gmail.com>
2023-07-20 16:00:37 -0300

    Comment unused component

--- react/training/workitout/src/App.js
+++ react/training/workitout/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 // import ColorList from './ColorList';
 import SlotMachine from './Slot';
-import Clicker from './Clicker';
+// import Clicker from './Clicker';
 
 function App() {
   return (
@@ -12,10 +12,12 @@ function App() {
       <SlotMachine val1="🥑" val2="🥑" val3="🥑" />
       <SlotMachine val1="🍒" val2="🍌" val3="🍌" />
       <SlotMachine val1="🍒" val2="🍒" val3="🍒" />
-      {/* <ColorList colors={["red", "pink", "indigo"]} />
-      <ColorList colors={["darkblue", "gold", "teal"]} /> */}
+      {/* <h2>Color list with props</h2>
+      <ColorList colors={["red", "pink", "indigo"]} />
+      <ColorList colors={["darkblue", "gold", "teal"]} />
+      <h2>Clicker component with useState</h2>
       <Clicker message="wohooo!" buttonText="Click here!" />
-      <Clicker message="stooop!" buttonText="Dont Click!" />
+      <Clicker message="stooop!" buttonText="Dont Click!" /> */}
     </div>
   );
 }
