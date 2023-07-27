46edf7c8b893a5df1050b4d0063b945b2d05ac86
kaenovsky <martinkaen@gmail.com>
2023-07-27 14:44:22 -0300

    Add EmojiClicker component

--- react/training/workitout/src/EmojiClicker.jsx
+++ react/training/workitout/src/EmojiClicker.jsx
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 
 export default function EmojiClicker() {
     
-    const emojiList = ["😐", "😓", "😅", "😍", "🙃", "😵", "🤒", "😡", "😨", "🤕", "😬"];
+    const emojiList = ["😐", "😓", "😅", "😍", "🙃", "😵", "🤒", "😡", "😨", "🤕", "😬", "🤢"];
     const randomEmoji = Math.floor(Math.random() * emojiList.length);
     
     const [emojis, setEmojis] = useState([{ id: uuid(), emoji: emojiList[randomEmoji] }]);
@@ -12,12 +12,33 @@ export default function EmojiClicker() {
         setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: emojiList[randomEmoji] }]);
     }
 
+    const deleteEmoji = (id) => {
+        setEmojis((prevEmojis) => {
+            return prevEmojis.filter((e => e.id !== id));
+        })        
+    } 
+
+    const makeThemHearts = () => {
+        const allHearts = emojis.map(e => {
+            return {
+                ...e,
+                emoji: "💜"
+            }                    
+        });
+        setEmojis(allHearts);
+    }
+
     return (
         <div style={{ margin: "2rem"}}>
             <button onClick={addEmoji} style={{ margin: "1rem"}}>Add emoji</button>
+            <button onClick={makeThemHearts} style={{ margin: "1rem"}}>Make all emoji hearts</button>
             <div>
                 {emojis.map((e) => (
-                    <span key={e.id} style={{ fontSize: "3rem" }}>
+                    <span
+                        onClick={() => deleteEmoji(e.id)} 
+                        key={e.id}
+                        style={{ fontSize: "3rem" }}
+                    >
                         {e.emoji}
                     </span>
                 ))}
