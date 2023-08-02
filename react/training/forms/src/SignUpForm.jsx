c0637f11d0a77f8308ac6a4a7bed736765181875
kaenovsky <martinkaen@gmail.com>
2023-08-02 17:30:45 -0300

    jsx +

--- react/training/forms/src/SignUpForm.jsx
+++ react/training/forms/src/SignUpForm.jsx
@@ -13,17 +13,35 @@ export default function SignUpForm() {
         });
     };
 
+    const handleSubmit = () => {
+        console.log(formData.firstName, formData.lastName);
+    }
+
+    const isRed = () => {
+        if (formData.firstName === 'red') {
+            return true;
+        }
+    }
+
+    const isBlue = () => {
+        if (formData.lastName === 'blue') {
+            return true;
+        }
+    }
+
     return (
         <div style={{ margin: "1rem" }}>
+            <h2 style={{ color: isRed() ? "red" : "black" }}>{formData.firstName}</h2>
+            <h2 style={{ color: isBlue() ? "blue" : "black" }}>{formData.lastName}</h2>
             <div>
                 <label htmlFor="firstName">First Name: </label>
-                <input type="text" name="firstName" id="firstName" value={formData.firstName} onChange={handleChange} />
+                <input type="text" name="firstName" id="firstName" value={formData.firstName} onChange={handleChange} />                
             </div>
             <div>
                 <label htmlFor="lastName">Last name: </label>
                 <input type="text" name="lastName" id="lastName" value={formData.lastName} onChange={handleChange} />
             </div>
-            <button>Send</button>
+            <button onClick={handleSubmit}>Send</button>
         </div>
     );
 }
